fcd886857314c4718ebfa77bd214b99db6f0c820
Kshitij Lauria <kslauria@gmail.com>
2018-03-08 16:37:21 -0500

    Control the frog with keys

--- jsoo/main.ml
+++ jsoo/main.ml
@@ -4,7 +4,7 @@ open Js_of_ocaml
    `##.prop_name := ` to set a property
    ##.prop_name to read (no deref)
 
-   put a table here ; bg here ; each cell has an id
+   keypress events should trigger a redraw.
 *)
 
 let get_by_id id =
@@ -15,12 +15,27 @@ let get_by_id id =
 let get_foo_div () = get_by_id "foo"
 ;;
 
+let set_cell row column img_path =
+    let img_id = (Int.to_string row) ^ "." ^ (Int.to_string column) in
+    let img_elem = get_by_id img_id in
+    img_elem##setAttribute (Js.string "src") (Js.string img_path);
+;;
+
+let render frog_row frog_col =
+  List.iter [0;1] ~f:(fun row ->
+      List.iter [0;1] ~f:(fun col ->
+          set_cell row col ""));
+  set_cell frog_row frog_col  "assets/frog-icon.png";
+;;
+
 let () =
   Dom_html.window##.onload := (Dom.handler (fun _ ->
       let foo_div = get_foo_div () in
       foo_div##.textContent := Js.Opt.return (Js.string "Hello, world!");
       Js._true
     ));
+  let frog_row = ref 1 in
+  let frog_col = ref 0 in
   Dom_html.window##.onkeydown := (Dom.handler (fun key_event ->
       let foo_div = get_foo_div () in
       let key = Option.value_exn (Js.Optdef.to_option (key_event##.key)) in
@@ -32,21 +47,18 @@ let () =
         | "ArrowRight" -> foo_div##.textContent := Js.Opt.return key
         | _            -> ()
       in
+      let () =
+        match Js.to_string key with
+        | "ArrowUp"   -> frog_row := if !frog_row = 0 then !frog_row else !frog_row - 1
+        | "ArrowDown" -> frog_row := if !frog_row = 1 then !frog_row else !frog_row + 1
+        | _ -> ()
+      in
+      render !frog_row !frog_col;
       Js._true));
-  let set_cell column img_path =
-    let img_id = "0." ^ (Int.to_string column) in
-    let img_elem = get_by_id img_id in
-    img_elem##setAttribute (Js.string "src") (Js.string img_path);
-  in
-  let frog_col = ref 0 in
-  let blank_col = ref 1 in
   let _ =
     Dom_html.window##setInterval (Js.wrap_callback (fun () ->
-        set_cell !frog_col  "assets/frog-icon.png";
-        set_cell !blank_col "";
-        let tmp = !frog_col in
-        frog_col := !blank_col;
-        blank_col := tmp
+        render !frog_row !frog_col;
+        frog_col := (!frog_col + 1) % 2;
       ))
       1000.0
   in
